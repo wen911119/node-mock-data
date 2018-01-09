17fafa06fe0a4efcf8286f705d544b7b998fbdbc
wen911119 <wen911119@gmail.com>
2018-01-09 11:02:58 +0800

    利用faker.js来mock数据

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 var express = require('express');
+var faker = require('faker');
 var app = express();
 
 app.post('/test', function (req, res) {
     console.log('qqqqqqqqq')
-  res.send('Hello World!');
-});
-
-app.post('/api/test', function (req, res) {
-    console.log('aaaaaa')
-  res.send('err');
+    let users = []
+    for (let i = 0; i < 10; i++) {
+        users.push({
+            name: faker.name.findName(),
+            email: faker.internet.email(),
+            avatar: faker.image.avatar()
+        })
+    }
+    res.json({ users });
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+    console.log('Example app listening on port 3000!');
 });
